76149744ec830b5b8f47e57044fa9d7b2979188f
WilfredoAaronSosaRamosBoushtech <wilfredo.sosa@boushtech.net>
2024-08-01 08:25:02 -0500

    new icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
